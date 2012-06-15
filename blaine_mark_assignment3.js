07f57771fa5a660ae2e8c9007cf6e3fbe64bc9eb
Mark Blaine <markopollo@unknown68a86d4158f4.gateway.2wire.net>
2012-06-14 20:30:45 -0500

    Added JSON data and defined some objects

--- blaine_mark_assignment3.js
+++ blaine_mark_assignment3.js
@@ -1,1 +1,25 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//JSON
+var campsite = {
+	"type": "VIP",
+	"width": 10,
+	"depth": 20,
+	"showers": true
+};
+
+var food = {
+	"pizza": "New York",
+	"drink": "water",
+	eat: function(){
+		var meal = "Let\'s get some " + this.pizza + " pizza";
+		return meal;
+	},
+	hydrate: function(){
+		var guzzle = "Let's drink some " + this.drink;
+		return guzzle;
+	}
+};
+
+console.log(food.eat());
+console.log(food.hydrate());
